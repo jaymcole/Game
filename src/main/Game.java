fa777930653749bfef70f6c1c307e6c256addc7b
Jason Cole <jaymcole2k@gmail.com>
2016-10-16 12:54:39 -0400

    Created Game.java + GameScreen.java

---
+++ src/main/Game.java
@@ -0,0 +1,78 @@
+package main;
+
+import java.awt.Graphics;
+
+public class Game {
+
+	public static final int UPS = 60;
+	public static final int FPS = 60;
+	public static final boolean RENDER_TIME = true;
+
+	private boolean running;
+	private GameScreen window;
+
+	public Game(GameScreen window) {
+		running = true;
+	}
+
+	public void run() {
+
+		long initialTime = System.nanoTime();
+		final double timeU = 1000000000 / UPS;
+		final double timeF = 1000000000 / FPS;
+		double deltaU = 0, deltaF = 0;
+		int frames = 0, ticks = 0;
+		long timer = System.currentTimeMillis();
+
+		while (running) {
+
+			long currentTime = System.nanoTime();
+			deltaU += (currentTime - initialTime) / timeU;
+			deltaF += (currentTime - initialTime) / timeF;
+			initialTime = currentTime;
+
+			if (deltaU >= 1) {
+				getInput();
+				update();
+				ticks++;
+				deltaU--;
+			}
+
+			if (deltaF >= 1) {
+				render();
+				frames++;
+				deltaF--;
+			}
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				if (RENDER_TIME) {
+					System.out.println(String.format("UPS: %s, FPS: %s", ticks, frames));
+				}
+				frames = 0;
+				ticks = 0;
+				timer += 1000;
+			}
+		}
+	}
+
+	private void getInput() {
+
+	}
+
+	private void update() {
+
+	}
+
+	private void render() {
+
+	}
+
+	public void paintComponent(Graphics g) {
+
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game(new GameScreen());
+		game.run();
+	}
+}
